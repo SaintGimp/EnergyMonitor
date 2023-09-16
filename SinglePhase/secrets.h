bec8705ef4ee994b0f12b19649750fac195ed485
Eric Lee <saintgimp@hotmail.com>
2023-09-15 21:34:10 -0700

    Add missing files

---
+++ SinglePhase/secrets.h
@@ -0,0 +1,6 @@
+char* wifiSSID = "<fill in>";
+char* wifiPassword = "<fill in>";
+
+const char* logstashUrl = "<fill in>";
+const char* keyHeader = "<fill in>";
+const char* logstashKey = "<fill in>";
---
+++ SplitPhase/secrets.h
@@ -0,0 +1,6 @@
+const char* wifiSSID = "<fill in>";
+const char* wifiPassword = "<fill in>";
+
+const char* logstashUrl = "<fill in>";
+const char* keyHeader = "<fill in>";
+const char* logstashKey = "<fill in>";
